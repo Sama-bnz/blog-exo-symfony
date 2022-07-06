673beef8ce314d64b0570e37b75d9dd5708c7246
Sama <thomasbenmezded@gmail.com>
2022-07-06 10:37:31 +0200

    update commentaires

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -4,15 +4,19 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
+ * Je crée un objet de Mapping du nom de ORM
  * @ORM\Entity()
  */
+//Je crée une classe et j'en definis le nom
 class Article
 {
+//    Je fournie à ma classe la valeur des colonnes
     /**
      * @ORM\Column(type="integer")
      * @ORM\Id()
      * @ORM\GeneratedValue()
      */
+//    Je donne le titre à ma colonne
     public $id;
 
     /**
