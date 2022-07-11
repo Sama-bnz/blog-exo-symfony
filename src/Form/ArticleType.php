768c8f69db185758d3ff2ad14b31494c4adabd0b
Sama <thomasbenmezded@gmail.com>
2022-07-11 16:21:35 +0200

    update add submit button

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,11 +13,13 @@ class ArticleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        //Le builder est ce qui va me permettre de construire le formulaire
         $builder
+            //genère l'input pour le title
             ->add('title')
             ->add('author')
             ->add('content')
-            ->add('isPublished')
+            ->add('submit',SubmitType::class)
         ;
     }
 
